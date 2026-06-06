82bc7cb0912fb6222b45518be4b4bd0dd4cd6b72
ringo81 <ringo81.rin5@gmail.com>
2026-06-06 21:05:06 +0900

    Improve recording flow and sharing

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "potsuri-v12";
+const CACHE_NAME = "potsuri-v14";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
